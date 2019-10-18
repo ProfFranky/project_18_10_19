1d518a6185297eb561cdf84df5c69ebd4d1d007e
ProfFranky <56717486+ProfFranky@users.noreply.github.com>
2019-10-18 12:18:48 +0200

    added classes

---
+++ Book.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibblioteca;
+
+/**
+ *
+ * @author francescosapioli
+ */
+public class Book {
+    private String title;
+    private String writer;
+    private String editor;
+    private String isbn;
+    private Double price;
+
+    
+    public Book() {
+        
+    }
+    
+    public Book(String title, String writer, String editor, String isbn, Double price) {
+        this.title = title;
+        this.writer = writer;
+        this.editor = editor;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String printIt(){
+        return title + " - " + writer + " - " + editor + " - " + isbn + " - " + price.toString();
+    }
+    
+    
+  
+    
+}
---
+++ Library.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibblioteca;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author francescosapioli
+ */
+public class Library {
+    
+    //final int MAX_BOOKS = 100;
+    
+    
+    private String name;
+    private int numBooks; //Used to notice how many books are in the library
+    private Map <String,Book> books;
+
+     
+
+    public Library(String name) {
+        this.books = new HashMap<String,Book>();
+        this.name = name;
+        this.numBooks = 0;
+        
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumBooks(int numBooks) {
+        this.numBooks = numBooks;
+    }
+
+   
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumBooks() {
+        return numBooks;
+    }
+
+    public Map<String, Book> getBooks() {
+        return books;
+    }
+
+    /* public int getMAX_BOOKS() {
+        return MAX_BOOKS;
+    }
+    */
+    
+    //TO-DO: Create a Method to add a book to the Library
+    public void addBook(Book a){
+        books.put(a.getTitle(), a);
+        numBooks++;
+        
+    }
+    
+    
+}
